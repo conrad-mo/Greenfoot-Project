58e1774cafcb26677bb3c5739391e754e837251e
F0lkl0re <conradmo78@hotmail.com>
2020-11-26 16:41:46 -0500

    Added a start page

---
+++ StartScreen.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StartScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartScreen extends World
+{
+
+    /**
+     * Constructor for objects of class StartScreen.
+     * 
+     */
+    public StartScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        Label start = new Label("Start", 100);
+        addObject(start, 300, 200);
+        if(Greenfoot.mouseClicked(start))
+        {
+            MyWorld world = new MyWorld();
+            Greenfoot.setWorld(world);
+        }
+    }
+}
